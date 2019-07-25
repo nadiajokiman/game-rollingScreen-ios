18744cc997df6079d574498ca10d5def50cc2937
nadiajokiman <49181280+nadiajokiman@users.noreply.github.com>
2019-07-25 15:40:40 -0700

    Add files via upload

---
+++ Side Scroller/Side ScrollerTests/Side_ScrollerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Side_ScrollerTests.swift
+//  Side ScrollerTests
+//
+//  Created by GWC2 on 7/23/19.
+//  Copyright © 2019 GWC2. All rights reserved.
+//
+
+import XCTest
+@testable import Side_Scroller
+
+class Side_ScrollerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
